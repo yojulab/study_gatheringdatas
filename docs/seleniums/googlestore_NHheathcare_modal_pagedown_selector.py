dcf09f125ba8d043d0232b7b6a1b438885b31a3f
Sanghun Oh <otter.oh@gmail.com>
2023-12-29 11:15:06 +0900

    add somethings

--- docs/seleniums/googlestore_NHheathcare_modal_pagedown_selector.py
+++ docs/seleniums/googlestore_NHheathcare_modal_pagedown_selector.py
@@ -13,35 +13,38 @@ browser = webdriver.Chrome(service=ChromeService(webdriver_manager_directory))
 capabilities = browser.capabilities
 
 # - 주소 입력(https://www.w3schools.com/)
-url = 'https://play.google.com/store/apps/details?id=com.nhlife.customer.healthcare&hl=ko-KR'
-browser.get(url)
+browser.get("https://play.google.com/store/apps/details?id=com.nhlife.customer.healthcare&hl=ko-KR&pli=1")
 
-# - 정보 획득
 from selenium.webdriver.common.by import By
-# 평가 및 리뷰 클릭
-selector_element = '#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div.wkMJlb.YWi3ub > div > div.qZmL0 > div:nth-child(1) > c-wiz:nth-child(4) > section > header > div > div:nth-child(2) > button > i'
+# 모달 화면 띄우기 : 평가 및 리뷰 클릭
+selector_element = "#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div.wkMJlb.YWi3ub > div > div.qZmL0 > div:nth-child(1) > c-wiz:nth-child(4) > section > header > div > div:nth-child(2) > button"
 browser.find_element(by=By.CSS_SELECTOR, value=selector_element).click()
 
-# modal 스크롤 영역 
-# document.querySelector('div.fysCi').style.overflow
-selector_element = 'div.fysCi'
-element_scrollable_div = browser.find_element(by=By.CSS_SELECTOR, value=selector_element)
+# - 정보 획득
+# 댓글 모달 확인 : (css overflow:scroll or auto) div.fysCi
+selector_element = "div.fysCi"
+element_scrollableDiv = browser.find_element(by=By.CSS_SELECTOR, value=selector_element)
 
-# 댓글 모두 표시까지 페이지를 다운합니다.
-# within javascript in browser
-# var scrollableDiv = document.querySelector('div.fysCi');
-# scrollableDiv.scrollTo(0, scrollableDiv.scrollHeight);
+
+# 스크롤 전 댓글 개수 확인 : div.RHo1pe
+selector_element = "div.RHo1pe"
+elements_comment = browser.find_elements(by=By.CSS_SELECTOR, value=selector_element)
+print("count comment before done scroll : {}".format(len(elements_comment)))
+
+# 댓글 마지막까지 스크롤 : scrollHeight 확인
+# - scrollableDiv.scrollHeight
+# - scrollableDiv.scrollTo(0, scrollableDiv.scrollHeight);
 previous_scrollHeight = 0
-
-# pagedown 전 댓글 갯수 확인 : div.RHo1pe
-elements_comment = browser.find_elements(by=By.CSS_SELECTOR, value='div.RHo1pe')
-print('count comment before done scroll : {}'.format(len(elements_comment)))
 while True:
-    browser.execute_script('arguments[0].scrollTo(arguments[1], arguments[0].scrollHeight);'
-                           , element_scrollable_div, previous_scrollHeight)
+    # python 방식 변수 매칭
+    # print("{0}.scrollTo({1}, {0}.scrollHeight);"
+    #       .format(element_scrollableDiv,previous_scrollHeight))
+    # javascript와 python 결합 방식 변수 매칭
+    browser.execute_script("arguments[0].scrollTo(arguments[1], arguments[0].scrollHeight);"
+                           ,element_scrollableDiv,previous_scrollHeight)
 
     current_scrollHeight = browser.execute_script("return arguments[0].scrollHeight"
-                                                  , element_scrollable_div)
+                                                  ,element_scrollableDiv)
     if previous_scrollHeight >= current_scrollHeight:
         break
     else :
@@ -49,11 +52,13 @@ while True:
     time.sleep(1)
     pass
 
-# pagedown 전 댓글 갯수 확인 : div.RHo1pe
-elements_comment = browser.find_elements(by=By.CSS_SELECTOR, value='div.RHo1pe')
-print('count comment after done scroll : {}'.format(len(elements_comment)))
+# 스크롤 후 댓글 개수 확인 : div.RHo1pe
+selector_element = "div.RHo1pe"
+elements_comment = browser.find_elements(by=By.CSS_SELECTOR, value=selector_element)
+print("count comment after done scroll : {}".format(len(elements_comment)))
 
 pass
+# browser.save_screenshot('./formats.png')
 
 # 브라우저 종료
 browser.quit()
