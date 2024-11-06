f49aea41f894d68b068356388accbeadfd4c8849
otter <otter.oh@gmail.com>
2024-11-06 02:42:16 +0000

    feat : add iframe

--- codes/seleniums/08_iframe_navercafe.py
+++ codes/seleniums/08_iframe_navercafe.py
@@ -1,4 +1,22 @@
+# * 웹 크롤링 동작
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+webdriver_manager_directory = ChromeDriverManager().install()
+
 def main():
+    # ChromeDriver 실행
+    browser = webdriver.Chrome(service=ChromeService(webdriver_manager_directory))
+
+    # Chrome WebDriver의 capabilities 속성 사용
+    capabilities = browser.capabilities
+
+    # - 주소 입력(https://www.w3schools.com/)
+    browser.get("https://cafe.naver.com/sbabellows")
+
     return 
 
 if __name__ == '__main__':
